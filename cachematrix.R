762c9b5ef7934f361c63d5d358c3a67ca77bd92f
Maria St Ivanova <ivanova.maria.st@gmail.com>
2016-11-13 14:58:51 +0100

    my solution to the 2nd R assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are nested within each other and 
+## together they allow the inverse of a matrix to be calculated and 
+## kept in the memory. If the same matrix is input for calculation
+## of the inverse, then the mean value will be pulled from the 
+## memory instead of being recalculated - this saves time. 
+## If a new/different matrix is input, then a new inverse
+## is calculated. 
 
-## Write a short comment describing this function
+## This first function creates an R object of the type "list" 
+## that stores a matrix and its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This second function takes as argument the list created
+## with the first function. It retrieves the inverse matrix
+## that is stored in the list's envitonment. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+## Return a matrix that is the inverse of 'x'
 }
+
+## You can test the two functions, after creating them, 
+## by running these lines: 
+
+maria <- matrix(rnorm(1:1000000), 1000, 1000)
+
+## the next line assures the matrix has inverse
+diag(maria) <- diag(maria) + 1
+
+maria1 <- makeCacheMatrix(maria)
+
+date(); o <- cacheSolve(maria1); date()
+## worked 4 sec
+
+date(); o <- cacheSolve(maria1); date()
+## worked less than 1 sec
+
